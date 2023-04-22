a302b434c332829f80986abffbaf72c1890a777b
Oleg <o.zhovtanskyi@gmail.com>
2023-04-22 18:39:09 +0200

    Added a sound effect for the end of the timer

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -6,23 +6,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    //MARK: - Main variables and constant
     let eggTime = ["softButton" : 240, "mediumButton": 360, "hardButton": 720]
     var currentTime = 0
     var choosenButton = 0
     var buttonText = ""
+    var player: AVAudioPlayer?
     
+    //MARK: - IBoutlets for buttons label and progress bar
     @IBOutlet weak var mainLabel: UILabel!
-    
     @IBOutlet weak var eggTimeProgress: UIProgressView!
-    
-    
     @IBOutlet weak var softButton: UIButton!
     @IBOutlet weak var mediumButton: UIButton!
     @IBOutlet weak var hardButton: UIButton!
     
-    
+    //MARK: - IBAction for action buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
         if sender == softButton {
             choosenButton = self.eggTime["softButton"]!
@@ -47,6 +48,7 @@ class ViewController: UIViewController {
             
             if self.currentTime == self.choosenButton {
                 DispatchQueue.main.async {
+                    self.playSound()
                     self.mainLabel.text = "Eggs are already cooked"
                 }
                 timer.invalidate()
@@ -58,9 +60,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         eggTimeProgress.progress = 0.0
     }
-
-
-
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "alarm", ofType: "mp3") else {return}
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            self.player?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
